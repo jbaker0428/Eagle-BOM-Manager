543eef5b6090333ce09f412a43f8113f5690aebf
Joe Baker <jbaker0428@gmail.com>
2012-03-31 23:42:44 -0400

    Refactoring: seperate method for each distributor website scrape.
    Signed-off-by: Joe Baker <jbaker0428@gmail.com>

--- urbm_product.py
+++ urbm_product.py
@@ -43,25 +43,6 @@ class Product:
 		self.series = ""
 		self.package = ""
 		self.db = database
-	
-	'''def __init__(self, vendor, vendor_pn, databaseFile):
-		with open(databaseFile, 'wb') as f:
-			db = csv.reader(f, delimiter=',', quotechar = '"', quoting=csv.QUOTE_ALL)
-			rownum = 0
-			for row in db:
-				if row[0] == vendor and row[1] == vendor_pn:
-					self.vendor = vendor
-					self.vendor_pn = vendor_pn
-					self.mfg_pn = row[2]
-					
-					# prices... need to split the string two ways
-					pricesStr = row[3].split(",")
-					for pair in pricesStr:
-						keyVal = pair.split(":")
-						self.prices[keyVal[0]] = keyVal[1]
-					
-					self.inventory = row[4]
-					self.datasheet = row[5]'''
 		
 	''' Returns the (price break, unit price) list pair for the given purchase quantity.
 	If qty is below the lowest break, the lowest is returned.'''
@@ -77,103 +58,106 @@ class Product:
 				return [breaks[i], self.prices[breaks[i]]]
 			elif  breaks[i] > qty:
 				return [breaks[i-1], self.prices[breaks[i-1]]]			
+	
+	def scrapeDK(self):
+		# Clear previous pricing data (in case price break keys change)
+		self.prices.clear()
 		
+		url = "http://search.digikey.com/us/en/products/soup/" + self.vendor_pn
+		page = urllib2.urlopen(url)
+		soup = BeautifulSoup(page)
+		print "URL: %s" % url
+		# Get prices
+		priceTable = soup.body('table', id="pricing")
+		# priceTable.contents[x] should be the tr tags...
+		for t in priceTable:
+			for r in t:
+				# r.contents should be td Tags... except the first!
+				if r == '\n':
+					pass
+				elif r.contents[0].name == 'th':
+					print "Found r.name == th"
+					#pass	# do nothing
+				else:
+					newBreakString = r.contents[0].string
+					# Remove commas
+					if newBreakString.isdigit() == False:
+						newBreakString = newBreakString.replace(",", "")
+					print "newBreakString is: %s" % newBreakString					
+					newBreak = int(newBreakString)
+					newUnitPrice = float(r.contents[1].string)
+					self.prices[newBreak] = newUnitPrice
+				
+		# Get inventory
+		# If the item is out of stock, the <td> that normally holds the
+		# quantity available will have a text input box that we need to
+		# watch out for
+		invSoup = soup.body('td', id="quantityavailable")
+		print "Length of form search results: %s" % len(invSoup[0].findAll('form'))
+		if len(invSoup[0].findAll('form')) > 0:
+			self.inventory = 0
+		
+		else:
+			invString = invSoup[0].contents[0]
+			if invString.isdigit() == False:
+				invString = invString.replace(",", "")
+			self.inventory = int(invString)
+		
+		# Get manufacturer and PN
+		self.manufacturer = soup.body('th', text="Manufacturer")[0].parent.nextSibling.contents[0].string
+		print "manufacturer is: %s" % self.manufacturer
+		self.mfg_pn = soup.body('th', text="Manufacturer Part Number")[0].parent.nextSibling.contents[0].string
+		print "mfg_pn is: %s" % self.mfg_pn
+		
+		# Get datasheet filename and download
+		datasheetSoup = soup.body('th', text="Datasheets")[0].parent.nextSibling
+		datasheetA = datasheetSoup.findAllNext('a')[0]
+		print "datasheetSoup is: %s" % datasheetSoup
+		print "datasheetA is: %s" % datasheetA
+		datasheetURL = datasheetA['href']
+		print "datasheetURL is: %s" % datasheetURL
+		
+		r = urllib2.urlopen(urllib2.Request(datasheetURL))
+		try:
+			fileName = getFileName(url,r)
+			self.datasheet = fileName;
+			# TODO: Do not re-download if already saved
+			with open(fileName, 'wb') as f:
+				shutil.copyfileobj(r,f)
+		finally:
+			r.close()
+		print "datasheet is: %s" % self.datasheet
+		# Get remaining strings (desc, category, family, series, package)
+		self.description = soup.body('th', text="Description")[0].parent.nextSibling.contents[0].string
+		print "description is: %s" % self.description
+		self.category = soup.body('th', text="Category")[0].parent.nextSibling.contents[0].string
+		print "category is: %s" % self.category
+		self.family = soup.body('th', text="Family")[0].parent.nextSibling.contents[0].string
+		print "family is: %s" % self.family
+		self.series = soup.body('th', text="Series")[0].parent.nextSibling.contents[0].string
+		print "series is: %s" % self.series
+		self.package = soup.body('th', text="Package / Case")[0].parent.nextSibling.contents[0].string
+		print "package is: %s" % self.package
+		
+		self.writeToDB()
+	
+	def scrapeSFE(self):
+		# Clear previous pricing data (in case price break keys change)
+		self.prices.clear()
+		
+		# The URL contains the numeric portion of the part number, minus any leading zeroes
+		url = "http://www.sparkfun.com/products/" + str(int(self.pn.split("-")))
+		page = urllib2.urlopen(url)
+		soup = BeautifulSoup(page)
+			
 	def scrape(self):
 		# Proceed based on vendor
-		#if self.vendor == self.vendors.DK:
 		if self.vendor == Product.VENDOR_DK:
-			# Clear previous pricing data (in case price break keys change)
-			self.prices.clear()
-			
-			url = "http://search.digikey.com/us/en/products/soup/" + self.vendor_pn
-			page = urllib2.urlopen(url)
-			soup = BeautifulSoup(page)
-			print "URL: %s" % url
-			# Get prices
-			priceTable = soup.body('table', id="pricing")
-			# priceTable.contents[x] should be the tr tags...
-			for t in priceTable:
-				for r in t:
-					# r.contents should be td Tags... except the first!
-					if r == '\n':
-						pass
-					elif r.contents[0].name == 'th':
-						print "Found r.name == th"
-						#pass	# do nothing
-					else:
-						newBreakString = r.contents[0].string
-						# Remove commas
-						if newBreakString.isdigit() == False:
-							newBreakString = newBreakString.replace(",", "")
-						print "newBreakString is: %s" % newBreakString					
-						newBreak = int(newBreakString)
-						newUnitPrice = float(r.contents[1].string)
-						self.prices[newBreak] = newUnitPrice
-					
-			# Get inventory
-			# If the item is out of stock, the <td> that normally holds the
-			# quantity available will have a text input box that we need to
-			# watch out for
-			invSoup = soup.body('td', id="quantityavailable")
-			print "Length of form search results: %s" % len(invSoup[0].findAll('form'))
-			if len(invSoup[0].findAll('form')) > 0:
-				self.inventory = 0
-			
-			else:
-				invString = invSoup[0].contents[0]
-				if invString.isdigit() == False:
-					invString = invString.replace(",", "")
-				self.inventory = int(invString)
-			
-			# Get manufacturer and PN
-			self.manufacturer = soup.body('th', text="Manufacturer")[0].parent.nextSibling.contents[0].string
-			print "manufacturer is: %s" % self.manufacturer
-			self.mfg_pn = soup.body('th', text="Manufacturer Part Number")[0].parent.nextSibling.contents[0].string
-			print "mfg_pn is: %s" % self.mfg_pn
-			
-			# Get datasheet filename and download
-			datasheetSoup = soup.body('th', text="Datasheets")[0].parent.nextSibling
-			datasheetA = datasheetSoup.findAllNext('a')[0]
-			print "datasheetSoup is: %s" % datasheetSoup
-			print "datasheetA is: %s" % datasheetA
-			datasheetURL = datasheetA['href']
-			print "datasheetURL is: %s" % datasheetURL
-			
-			r = urllib2.urlopen(urllib2.Request(datasheetURL))
-			try:
-				fileName = getFileName(url,r)
-				self.datasheet = fileName;
-				# TODO: Do not re-download if already saved
-				with open(fileName, 'wb') as f:
-					shutil.copyfileobj(r,f)
-			finally:
-				r.close()
-			print "datasheet is: %s" % self.datasheet
-			# Get remaining strings (desc, category, family, series, package)
-			self.description = soup.body('th', text="Description")[0].parent.nextSibling.contents[0].string
-			print "description is: %s" % self.description
-			self.category = soup.body('th', text="Category")[0].parent.nextSibling.contents[0].string
-			print "category is: %s" % self.category
-			self.family = soup.body('th', text="Family")[0].parent.nextSibling.contents[0].string
-			print "family is: %s" % self.family
-			self.series = soup.body('th', text="Series")[0].parent.nextSibling.contents[0].string
-			print "series is: %s" % self.series
-			self.package = soup.body('th', text="Package / Case")[0].parent.nextSibling.contents[0].string
-			print "package is: %s" % self.package
-			
-			self.writeToDB()
-			# TODO: Write to persistent database
+			self.scrapeDK()
 		elif self.vendor == Product.VENDOR_ME:
 			pass
-		
 		elif self.vendor == Product.VENDOR_SFE:
-			# Clear previous pricing data (in case price break keys change)
-			self.prices.clear()
-			
-			# The URL contains the numeric portion of the part number, minus any leading zeroes
-			url = "http://www.sparkfun.com/products/" + str(int(self.pn.split("-")))
-			page = urllib2.urlopen(url)
-			soup = BeautifulSoup(page)
+			self.scrapeSFE()
 		else:
 			print 'Error: %s has invalid vendor: %s' % (self.pn, self.vendor)
 
